2cf84090d014a94a1db482fbcd77db0dfe3abec0
Elijah Sinkamba <chisanga110@live.com>
2022-10-13 08:52:07 +0200

    Python Imports & modules

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+def add(a, b):
+    """My addition function
+    
+        Args:
+            a (int): first number
+            b (int): second number
+    
+        Returns:
+            int: sum of a and b
+        """
+    return a + b
